e41a979408b2a6259452b0ccbbce39c6415679cf
Philipp Duck <phil.mar.du@gmail.com>
2024-12-05 20:04:03 +0100

    Base Layout

--- SaveTheNation/GameScene.swift
+++ SaveTheNation/GameScene.swift
@@ -1,21 +1,48 @@
-//
-//  GameScene.swift
-//  SaveTheNation
-//
-//  Created by Philipp-Marcel Duck on 05.12.24.
-//
-
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-
     override func didMove(to view: SKView) {
-        
+        backgroundColor = .black
 
+        // Platzhalter: Grid für Bevölkerungsgruppen
+        let populationGrid = SKNode()
+        populationGrid.position = CGPoint(x: size.width / 2, y: size.height * 0.85) // Positionierung in der oberen Hälfte
+        addChild(populationGrid)
+
+        // Dynamische Berechnung der Breite und Positionen
+        let groupCount = 6 // Anzahl der Gruppen
+        let margin: CGFloat = 15 // Margin links und rechts
+        let gap: CGFloat = 5 // Abstand zwischen den Objekten
+
+        // Berechnung der Breite jedes Kästchens
+        let totalGap = gap * CGFloat(groupCount - 1) // Gesamtbreite der Gaps
+        let availableWidth = size.width - (2 * margin) - totalGap // Verfügbare Breite für Kästchen
+        let itemWidth = availableWidth / CGFloat(groupCount) // Dynamische Breite jedes Objekts
+        let itemSize = CGSize(width: itemWidth, height: 80) // Quadratische Kästchen
+
+        // Platzhalter-Icons für Bevölkerungsgruppen hinzufügen
+        for i in 0..<groupCount {
+            let groupPlaceholder = SKSpriteNode(color: .darkGray, size: itemSize)
+
+            // Position der Objekte im Grid
+            let xPosition = margin + (itemWidth / 2) + (CGFloat(i) * (itemWidth + gap)) - (size.width / 2)
+            groupPlaceholder.position = CGPoint(x: xPosition, y: 0)
+
+            populationGrid.addChild(groupPlaceholder)
+        }
+
+        // Platzhalter: Aktuelle Karte
+        let cardPlaceholder = SKSpriteNode(color: .darkGray, size: CGSize(width: size.width - (2 * margin), height: 500))
+        cardPlaceholder.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(cardPlaceholder)
+
+        // Platzhalter: Monatszähler
+        let monthLabel = SKLabelNode(text: "Regierungszeit: 0")
+        monthLabel.fontSize = 24
+        monthLabel.fontColor = .white
+        monthLabel.position = CGPoint(x: size.width / 2, y: size.height * 0.1)
+        addChild(monthLabel)
     }
-    
-    
 
 }
--- SaveTheNation/GameViewController.swift
+++ SaveTheNation/GameViewController.swift
@@ -1,45 +1,26 @@
 //
 //  GameViewController.swift
-//  SaveTheNation
+//  BreadAndGames
 //
-//  Created by Philipp-Marcel Duck on 05.12.24.
+//  Created by Philipp-Marcel Duck on 22.11.24.
 //
 
-import UIKit
 import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+            // Lade die GameScene
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
     }
 }
